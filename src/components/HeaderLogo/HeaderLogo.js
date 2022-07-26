9aa8c9e5cc62a0720d629302f95f49f0082fdf60
Can Ocalir <can.ocalir@gmail.com>
2022-07-26 22:05:24 +0300

    Logo link added

--- src/components/HeaderLogo/HeaderLogo.js
+++ src/components/HeaderLogo/HeaderLogo.js
@@ -3,9 +3,13 @@ import logo from "./React-icon.png";
 
 export default function HeaderLogo() {
   return (
+    <>
+    <a href="/">
     <div className="header-logo">
-      <img src={logo} alt="" />
+     <img src={logo} alt="logo" />
       <h1>DevNews</h1>
     </div>
+    </a>
+     </>
   );
 }
